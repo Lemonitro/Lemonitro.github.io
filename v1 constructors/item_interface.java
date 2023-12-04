9f68d66a7c86cc5e5d08dcc44d295adc34ca9292
Engineering <Engineering@Helpdesk-LoanerT480.cls-edu.charlottelatin.org>
2023-12-04 08:57:18 -0500

    Made item interface + will work on it today, it will house basic functions for the items, and will help implement a bridge between buildings and items to create craft recipes and a basic game.... WE GO THIS WE BALLING!!!

---
+++ v1 constructors/item_interface.java
@@ -0,0 +1,13 @@
+public interface item_interface{
+    //Internal item interface
+    //Has all functions declared for easier implementation
+    
+    //Info
+
+    //Collect
+
+    //Craft
+
+    //Destroy/Thrash 
+
+}
